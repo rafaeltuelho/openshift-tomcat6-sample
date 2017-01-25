90a4752d05125b8616d315a478be262be7d72b55
rafaeltuelho <rafaelcba@gmail.com>
2017-01-25 15:46:46 -0200

    changes on Dockerfile and openshift template

---
+++ openshift/jenkins/Jenkinsfile.groovy
@@ -0,0 +1,23 @@
+def appName="openshift-tomcat6-sample"
+def project=""
+node {
+  stage("Initialize") {
+    project = env.PROJECT_NAME
+  }
+}
+node("maven") {
+  stage("Checkout src code from GOGs") {
+    git url: "http://gogs-cicd.cloud.rramalho.com/gogs/openshift-tomcat6-sample.git", branch: "master"
+  }
+  stage("Build WAR") {
+    sh "mvn clean package deploy -Popenshift-tomcat6 -s openshift/maven/settings.xml"
+  }
+}
+node {
+  stage("Build Image") {
+    sh "oc start-build ${appName}-docker --follow -n ${project}"
+  }
+  stage("Deploy") {
+    openshiftDeploy deploymentConfig: appName, namespace: project
+  }
+}
